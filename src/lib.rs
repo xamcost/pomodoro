86876db207e647a9638f4d79cc75b0e482c8864c
Maxime Costalonga <maxime.costalonga.external@eviden.com>
2025-04-19 13:37:49 +0100

    maint: move backend logic in library crate

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::fmt;
+use std::time;
+
+pub struct Timer {
+    duration: time::Duration,
+    start_time: Option<time::Instant>,
+}
+
+impl Timer {
+    pub fn new(minutes: u64) -> Self {
+        let duration = time::Duration::from_secs(minutes * 60);
+        Timer {
+            duration,
+            start_time: None,
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.start_time = Some(time::Instant::now());
+    }
+
+    pub fn elapsed(&self) -> time::Duration {
+        match self.start_time {
+            Some(start_time) => start_time.elapsed(),
+            None => time::Duration::from_secs(0),
+        }
+    }
+
+    pub fn remaining(&self) -> time::Duration {
+        if self.elapsed() >= self.duration {
+            return time::Duration::from_secs(0);
+        }
+        self.duration - self.elapsed()
+    }
+}
+
+impl fmt::Display for Timer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let remaining = self.remaining();
+        let (minutes, seconds) = get_min_sec_from_duration(remaining);
+        write!(f, "{:02}:{:02}", minutes, seconds)
+    }
+}
+
+fn get_min_sec_from_duration(duration: time::Duration) -> (u64, u64) {
+    let total_seconds = duration.as_secs();
+    let minutes = total_seconds / 60;
+    let seconds = total_seconds % 60;
+    (minutes, seconds)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use crossterm::event::{self, Event};
 use ratatui::{buffer, layout, style::Stylize, symbols, text, widgets, DefaultTerminal, Frame};
 use std::io;
-use std::time;
 
 fn main() -> io::Result<()> {
     let terminal = ratatui::init();
@@ -12,43 +11,16 @@ fn main() -> io::Result<()> {
 }
 
 pub struct App {
-    start_time: Option<time::Instant>,
-    work_duration: time::Duration,
+    timer: pomodoro::Timer,
     exit: bool,
-}
-
-fn get_min_sec_from_duration(duration: time::Duration) -> (u64, u64) {
-    let total_seconds = duration.as_secs();
-    let minutes = total_seconds / 60;
-    let seconds = total_seconds % 60;
-    (minutes, seconds)
 }
 
 impl App {
     pub fn new() -> Self {
         App {
-            start_time: Some(time::Instant::now()),
-            work_duration: time::Duration::from_secs(10),
+            timer: pomodoro::Timer::new(25),
             exit: false,
         }
-    }
-
-    fn start(&mut self) {
-        self.start_time = Some(time::Instant::now());
-    }
-
-    fn elapsed(&self) -> time::Duration {
-        match self.start_time {
-            Some(start_time) => start_time.elapsed(),
-            None => time::Duration::from_secs(0),
-        }
-    }
-
-    fn remaining(&self) -> time::Duration {
-        if self.elapsed() >= self.work_duration {
-            return time::Duration::from_secs(0);
-        }
-        self.work_duration - self.elapsed()
     }
 
     fn run(&mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
@@ -76,6 +48,9 @@ impl App {
 
     fn handle_key_event(&mut self, key_event: event::KeyEvent) {
         match key_event.code {
+            event::KeyCode::Char('s') => {
+                self.timer.start();
+            }
             event::KeyCode::Esc => self.exit = true,
             event::KeyCode::Char('q') => self.exit = true,
             _ => (),
@@ -86,16 +61,20 @@ impl App {
 impl widgets::Widget for &App {
     fn render(self, area: layout::Rect, buf: &mut buffer::Buffer) {
         let title = text::Line::from(" Pomodoro ".bold());
-        let instructions = text::Line::from(vec![" Quit ".into(), "<Q/Esc> ".blue().bold()]);
+        let instructions = text::Line::from(vec![
+            "Start ".into(),
+            "<S>".blue().bold(),
+            " Quit ".into(),
+            "<Q/Esc> ".blue().bold(),
+        ]);
         let block = widgets::Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())
             .border_set(symbols::border::THICK);
 
-        let (min, sec) = get_min_sec_from_duration(self.remaining());
         let counter_text = text::Text::from(vec![text::Line::from(vec![
             "Value: ".into(),
-            format!("{:0>2}:{:0>2}", min, sec).to_string().yellow(),
+            format!("{}", self.timer).yellow(),
         ])]);
 
         widgets::Paragraph::new(counter_text)
